b13f382441b8694f0ab4b83d95d5de9fce5a9612
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-03-24 06:33:57 +0300

    Update start-end.py

--- MatrixMusic/plugins/play/start-end.py
+++ MatrixMusic/plugins/play/start-end.py
@@ -6,17 +6,17 @@ from MatrixMusic import app
 
 @app.on_message(filters.video_chat_started)
 async def brah(client, message):
-       await message.reply("↯︙تم تشغيل ↫ ⦗ المحادثة المرئية ⦘")
+       await message.reply("<b>❆︙تم تشغيل ↫ ⦗ المحادثة المرئية ⦘</b>")
 @app.on_message(filters.video_chat_ended)
 async def brah2(client, message):
-       await message.reply("↯︙تم ايقاف ↫ ⦗ المحادثة المرئية ⦘")
+       await message.reply("<b>❆︙تم ايقاف ↫ ⦗ المحادثة المرئية ⦘</b>")
 @app.on_message(filters.video_chat_members_invited)
 async def fuckoff(client, message):
-           text = f"↯︙قام الشخص ↫ ⦗ {message.from_user.mention} ⦘"
+           text = f"<b>❆︙قام الشخص ↫</b> ⦗ {message.from_user.mention} ⦘"
            x = 0
            for user in message.video_chat_members_invited.users:
              try:
-               text += f"\n↯︙بدعوة شخص الى المحادثة المرئية ↫ ⦗ {user.first_name} ⦘"
+               text += f"\n<b>❆︙بدعوة شخص الى المحادثة المرئية </b> ⦗ {user.first_name} ⦘"
                x += 1
              except Exception:
                pass
