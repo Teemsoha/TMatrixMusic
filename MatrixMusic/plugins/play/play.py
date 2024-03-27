f70fa5d37386e7ff73997ad78d4354d1ca757457
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-03-27 21:06:39 +0300

    Update play.py

--- MatrixMusic/plugins/play/play.py
+++ MatrixMusic/plugins/play/play.py
@@ -26,26 +26,7 @@ from MatrixMusic.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 
-force_btn = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton(
-                text="ليطمـ𓆰ٰ⍣⃟ٰٰٖٖ۪۬🇾🇪۬ـئن عقلـ۬ۦٕ٘۬ﹻٰ۬ۛۛـي", url="https://t.me/My1mind1"
-            ),                        
-        ],        
-    ]
-)
 
-async def check_is_joined(message):    
-    try:
-        userid = message.from_user.id
-        status = await app.get_chat_member("My1mind1", userid)
-        return True
-    except Exception:
-        await message.reply_text("<b>❆︰ عذراً، عليك الانضمام الى هذهِ القناة أولاً  </b>\n<b>❆︰ اشترك ثم أرسل : شغل + اسم الاغنيه</b>",reply_markup=force_btn)
-        return False
-
-#gooooo
 
 @app.on_message(
     command(
@@ -77,8 +58,6 @@ async def play_commnd(
     url,
     fplay,
 ):
-    if not await check_is_joined(message):
-        return
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
     )
